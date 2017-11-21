c145a743fea39b5e3c12b8d449e196228addff20
Piou-piou <pilloud.anthony@gmail.com>
2017-11-21 13:14:39 +0100

    adding sass management with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,40 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: './source/js/ribs-checkbox.js',
-  output: {
-    filename: 'dist/js/ribs-checkbox.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        include: path.join(__dirname, 'assets/'),
-        loader: 'babel-loader',
-      }
+    entry: './source/js/ribs-checkbox.js',
+    output: {
+        filename: 'dist/js/ribs-checkbox.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'assets/'),
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    {loader: 'css-loader', options: {importLoaders: 1}},
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: (loader) => [
+                                require('autoprefixer')({
+                                    browsers: ["last 2 versions", "ie > 8"]
+                                })
+                            ]
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin(),
     ]
-  },
-  plugins: [
-    new UglifyJsPlugin(),
-  ]
 };
