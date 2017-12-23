084c021ba9794e7503115326e4569562a7d90c6a
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-12-23 19:32:54 +0100

    [func] add beginning of switched checkbox without jQuery

--- source/js/ribs-checkbox.js
+++ source/js/ribs-checkbox.js
@@ -1,37 +1,56 @@
 /*import $ from 'jQuery'
 
-$(document).ready(function() {
-  $(".checkbox-perso.rounded").each(function() {
-    var $input = $("input", this);
-    
-    $input.after('<span></span>');
-  })
-  
-  $(".checkbox-perso.switched").each(function() {
-    var $input = $("input", this);
-    
-    $input.hide().wrap('<span class="switch">');
-    $input.after('<span class="switch-container"> <span class="on">OUI</span> <span class="mid"></span> <span class="off">NON</span> </span>');
-  });
-});*/
+ $(document).ready(function() {
+ $(".checkbox-perso.rounded").each(function() {
+ var $input = $("input", this);
+
+ $input.after('<span></span>');
+ })
+
+ $(".checkbox-perso.switched").each(function() {
+ var $input = $("input", this);
+
+ $input.hide().wrap('<span class="switch">');
+ $input.after('<span class="switch-container"> <span class="on">OUI</span> <span class="mid"></span> <span class="off">NON</span> </span>');
+ });
+ });*/
 
 class RibsCheckbox {
   constructor() {
     this.roundedCheckboxs = document.getElementsByClassName('ribs-checkbox rounded');
+    this.switchedCheckboxs = document.getElementsByClassName('ribs-checkbox switched');
 
     if (this.roundedCheckboxs.length > 0) {
       this.initRounded();
     }
+
+    if (this.switchedCheckboxs.length > 0) {
+      this.initSwitched();
+    }
   }
 
   /**
-   * function that add a span element afeter input in rounded div
+   * function that add a span element after input in rounded div
    * to init rounded checkbox
    */
   initRounded() {
     Array.from(this.roundedCheckboxs).forEach((element, index) => {
       const input = element.querySelector('input');
+
       input.insertAdjacentHTML('afterend', '<span></span>');
+    });
+  }
+
+  /**
+   * function that add a span element after input in switched div and hide input
+   * to init switched checkbox
+   */
+  initSwitched() {
+    Array.from(this.switchedCheckboxs).forEach((element, index) => {
+      const input = element.querySelector('input');
+
+      input.style.display = 'none';
+      input.insertAdjacentHTML('afterend', '<span class="switch-container"> <span class="on">OUI</span> <span class="mid"></span> <span class="off">NON</span> </span>');
     });
   }
 }
