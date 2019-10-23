a5deae300833b5cbbf486a2258f373a44588c673
AnthonyPilloud <anthony.pilloud@quarks.pro>
2019-10-23 18:49:40 +0200

    [func] add js for checked checkbox

--- source/js/ribs-checkbox.js
+++ source/js/ribs-checkbox.js
@@ -3,10 +3,15 @@ import RibsCore from 'ribs-core';
 class RibsCheckbox {
   constructor() {
     this.roundedCheckboxs = document.getElementsByClassName('ribs-checkbox rounded');
+    this.checkedCheckboxs = document.getElementsByClassName('ribs-checkbox checked');
     this.switchedCheckboxs = document.getElementsByClassName('ribs-checkbox switched');
 
     if (this.roundedCheckboxs.length > 0) {
       this.initRounded();
+    }
+
+    if (this.checkedCheckboxs.length > 0) {
+      this.initChecked();
     }
 
     if (this.switchedCheckboxs.length > 0) {
@@ -21,6 +26,18 @@ class RibsCheckbox {
   initRounded() {
     Array.from(this.roundedCheckboxs).forEach((element, index) => {
       RibsCore.wrap(element, 'label', 'ribs-checkbox rounded');
+
+      element.insertAdjacentHTML('afterend', '<span></span>');
+    });
+  }
+
+  /**
+   * function that add a span element after input in checked div
+   * to init checked checkbox
+   */
+  initChecked() {
+    Array.from(this.checkedCheckboxs).forEach((element, index) => {
+      RibsCore.wrap(element, 'label', 'ribs-checkbox checked');
 
       element.insertAdjacentHTML('afterend', '<span></span>');
     });
